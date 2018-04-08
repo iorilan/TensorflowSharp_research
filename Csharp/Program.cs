a3ad2fa335161ed66091fd31c8b48ff4b0455468
iorilan <iorilan@hotmail.com>
2018-04-08 17:39:33 +0800

    c

---
+++ Csharp/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TensorFlow;
+
+namespace Csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var session = new TFSession())
+            {
+                var graph = session.Graph;
+
+                var a = graph.Const(2);
+                var b = graph.Const(3);
+                Console.WriteLine("a=2 b=3");
+
+                // Add two constants
+                var addingResults = session.GetRunner().Run(graph.Add(a, b));
+                var addingResultValue = addingResults.GetValue();
+                Console.WriteLine("a+b={0}", addingResultValue);
+
+                // Multiply two constants
+                var multiplyResults = session.GetRunner().Run(graph.Mul(a, b));
+                var multiplyResultValue = multiplyResults.GetValue();
+                Console.WriteLine("a*b={0}", multiplyResultValue);
+
+                Console.WriteLine("All Setup");
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ Ts_ImageClassification/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using TensorFlow;
+
+namespace Ts_ImageClassification
+{
+    /// <summary>
+    /// 1. install Tensorflowsharp
+    /// 2.download tensorflowlib.dll from 
+    /// http://ci.tensorflow.org/view/Nightly/job/nightly-libtensorflow-windows/lastSuccessfulBuild/artifact/lib_package/libtensorflow-cpu-windows-x86_64.zip
+    /// </summary>
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+           
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            var graph = new TFGraph();
+            var model = File.ReadAllBytes("../../ml_model/model.pb");
+            var labels = File.ReadAllLines("../../ml_model/labels.txt");
+            graph.Import(model);
+
+            var bestIdx = 0;
+            float best = 0;
+
+
+            var paths = Directory.GetFiles("../../img");
+
+            using (var session = new TFSession(graph))
+            {
+                foreach (var path in paths)
+                {
+                    var tensor = ImageUtil.CreateTensorFromImageFile(path);
+                    var runner = session.GetRunner();
+                    runner.AddInput(graph["Placeholder"][0], tensor).Fetch(graph["loss"][0]);
+                    var output = runner.Run();
+                    var result = output[0];
+
+                    var probabilities = ((float[][])result.GetValue(jagged: true))[0];
+                    for (int i = 0; i < probabilities.Length; i++)
+                    {
+                        if (probabilities[i] > best)
+                        {
+                            bestIdx = i;
+                            best = probabilities[i];
+                        }
+                    }
+
+
+
+
+                    // fin
+                    stopwatch.Stop();
+                    Console.WriteLine($"result of image [{path}] = {labels[bestIdx]} ({best * 100.0}%)");
+                    Console.WriteLine($"Total time: {stopwatch.Elapsed}");
+                    Console.ReadKey();
+
+                }
+
+
+            }
+
+        }
+    }
+}
